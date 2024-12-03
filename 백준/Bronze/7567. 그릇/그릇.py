9a5bf75e17e183019c86b67c64e573170ddaef98
Yuri Hong <123566658+glasshong@users.noreply.github.com>
2024-12-03 11:32:51 +0900

    [Bronze II] Title: 그릇, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/7567. 그릇/그릇.py
+++ 백준/Bronze/7567. 그릇/그릇.py
@@ -1,14 +1,8 @@
 a = list(input())
 b = 10
 for i in range(1, len(a)):
-    if a[i-1] == '(':
-        if a[i] == '(':
-            b += 5
-        else:
-            b += 10
-    elif a[i-1] == ')':
-        if a[i] == ')':
-            b += 5
-        else:
-            b += 10
+    if a[i-1] == a[i]:
+        b += 5
+    else:
+        b += 10
 print(b)
