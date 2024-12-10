cf705f5b0d7de816233404a3686dde0928aaad1c
Yuri Hong <123566658+glasshong@users.noreply.github.com>
2024-12-11 01:07:17 +0900

    [Bronze II] Title: 대표값, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2592. 대표값/대표값.py
+++ 백준/Bronze/2592. 대표값/대표값.py
@@ -1,6 +1,5 @@
-import statistics
 l = []
 for _ in range(10):
     l.append(int(input()))
-print(statistics.mean(l))
-print(statistics.mode(l))
+print(int(sum(l)/10))
+print(max(set(l), key=l.count))
