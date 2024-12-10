493332ec4b4c8be2119fe88bc8231ab7612a1b7a
Yuri Hong <123566658+glasshong@users.noreply.github.com>
2024-12-11 00:14:26 +0900

    [Bronze II] Title: 상수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,2 +1,2 @@
-A, B = map(str, input().split())
-print(max(int(A[2])*100 + int(A[1])*10 + int(A[0]), int(B[2])*100 + int(B[1])*10 + int(B[0])))
+A, B = input().split()
+print(max(A[::-1], B[::-1]))
