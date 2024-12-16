82d20acaf7a61f4d678388a21e5648d0a340bc91
Yuri Hong <123566658+glasshong@users.noreply.github.com>
2024-12-16 10:43:12 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,9 +1,1 @@
-s = input()
-l = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-f = []
-for i in range(len(l)):
-    if l[i] in s:
-        f.append(s.index(l[i]))
-    else:
-        f.append(-1)
-print(*f)
+print(' '.join(map(str, map(input().find, 'abcdefghijklmnopqrstuvwxyz'))))
