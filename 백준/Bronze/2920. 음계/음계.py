5829ec54d08495fef456cd7bb397d2b327223fbd
Yuri Hong <123566658+glasshong@users.noreply.github.com>
2024-12-21 23:46:23 +0900

    [Bronze II] Title: 음계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,7 +1,7 @@
 l = list(map(int, input().split()))
-if l == [1, 2, 3, 4, 5, 6, 7, 8]:
+if l == sorted(l):
     print('ascending')
-elif l == [8, 7, 6, 5, 4, 3, 2, 1]:
+elif l == sorted(l, reverse=True):
     print('descending')
 else:
     print('mixed')
