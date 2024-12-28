dff6e0e869f1b4e5ae6abb0260275aaff8b59514
Yuri Hong <123566658+glasshong@users.noreply.github.com>
2024-12-28 21:39:13 +0900

    [Silver III] Title: 통계학, Time: 580 ms, Memory: 72600 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,21 +1,10 @@
 import sys
 import statistics
 
-def my_round(val):
-    if abs(val) - abs(int(val)) >= 0.5:
-        if val >= 0:
-            return int(val) + 1
-        else:
-            return int(val) - 1
-    else:
-        return int(val)
-
 N = int(sys.stdin.readline())
-l = []
-for _ in range(N):
-    l.append(int(sys.stdin.readline()))
+l = [int(sys.stdin.readline()) for _ in range(N)]
 
-print(my_round(statistics.mean(l)))
+print(round(statistics.mean(l)))
 print(statistics.median(l))
 m = statistics.multimode(l)
 m.sort()
