b4c24a92bebb0863727e213582758b18e331a083
Yuri Hong <123566658+glasshong@users.noreply.github.com>
2024-12-28 22:00:23 +0900

    [Silver III] Title: 통계학, Time: 408 ms, Memory: 55524 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,15 +1,17 @@
 import sys
-import statistics
-
+from collections import Counter
 N = int(sys.stdin.readline())
 l = [int(sys.stdin.readline()) for _ in range(N)]
+l.sort()
 
-print(round(statistics.mean(l)))
-print(statistics.median(l))
-m = statistics.multimode(l)
-m.sort()
-if len(m) >= 2:
-    print(m[1])
+print(round(sum(l)/N))
+print(l[N//2])
+new_l = sorted(Counter(l).items(), key = lambda x : (-x[1], x[0]))
+if N == 1:
+    print(l[0])
 else:
-    print(m[0])
+    if new_l[0][1] != new_l[1][1]:
+        print(new_l[0][0])
+    else:
+        print(new_l[1][0])
 print(max(l)-min(l))
