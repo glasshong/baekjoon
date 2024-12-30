6b046b8ecef2947c577ecbfe3b05d6ac22610072
Yuri Hong <123566658+glasshong@users.noreply.github.com>
2024-12-30 11:15:27 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 204 ms, Memory: 48452 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,7 +1,8 @@
-N, M = map(int, input().split())
+import sys
+N, M = map(int, sys.stdin.readline().split())
 l = {}
 for _ in range(N):
-    d, p = input().split()
+    d, p = sys.stdin.readline().split()
     l[d] = p
 for _ in range(M):
-    print(l[input()])
+    print(l[sys.stdin.readline().strip()])
