917a3dc1abc19b50435c250db73e4e45fffdd5fa
Yuri Hong <123566658+glasshong@users.noreply.github.com>
2025-01-10 18:49:42 +0900

    [Silver V] Title: 단어 정렬, Time: 68 ms, Memory: 37024 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,18 +1,14 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
-l = {}
-
+l = []
 for _ in range(N):
-    a = input() 
-    b = len(a)
-    if b not in l:
-        l[b] = []
-    l[b].append(a)
+    l.append(input().strip())
+l = list(set(l))
 
-for k in l:
-    l[k] = sorted(set(l[k]))
+l.sort()
+l.sort(key=len)
 
-s = dict(sorted(l.items(), key=lambda x: x[0]))
-
-for k, ws in s.items():
-    for w in ws:
-        print(w)
+for i in l:
+	print(i)
