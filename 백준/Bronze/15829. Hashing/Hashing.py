deca78f7cfd4a75dbc239cbc9bafe6fa7e0234f0
Yuri Hong <123566658+glasshong@users.noreply.github.com>
2025-01-26 23:26:47 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,13 @@
+l = int(input())
+s = input()
+
+d = {}
+p = 'abcdefghijklmnopqrstuvwxyz'
+for i in range(1, 27):
+    d[p[i-1]] = i
+
+t = 0
+for j in range(l):
+    t += d[s[j]] * (31 ** j)
+
+print(t)
