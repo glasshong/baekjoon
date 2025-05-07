78ce7bdb060a957e9c8623b8c21fc0a3777a5ea9
Yuri Hong 홍유리 <yurihong18@gmail.com>
2025-05-07 21:27:51 +0900

    [Gold V] Title: 두 배 더하기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/12931. 두 배 더하기/두 배 더하기.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = [0] * n
+b = list(map(int, input().split()))
+
+count = 0
+while True:
+    if a == b:
+        break
+    else:
+        if all(i % 2 == 0 for i in b):
+            for i in range(n):
+                b[i] = b[i] // 2
+            count += 1
+        else:
+            for i in range(n):
+                if b[i] % 2 == 1:
+                    b[i] = b[i] - 1
+                    count += 1
+                else:
+                    pass
+
+print(count)
